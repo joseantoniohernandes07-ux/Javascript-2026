7f857030c5ca7e0286a24c3db1b1083c1d9961fe
unknown <230435186+joseantoniohernandes07-ux@users.noreply.github.com>
2026-03-09 11:03:49 -0600

    Aprendimos Comentarios y Variables

---
+++ js/01-comentarios.js
@@ -0,0 +1,30 @@
+/* 
+
+  Comentarios JS
+
+  Son lineas de texto que no ayudan a la documentacion del codigo
+
+  1. Comentarios de una linea > //
+  2. Comentarios de bloque > Ctrl + Shift + A
+
+*/
+
+/* 
+
+  Esto es un comentario de Bloque
+
+*/
+
+// Esto es un comentario de linea
+// Esto es otro comentario
+// console.log("Hola mamá")
+
+/* alert("Ya tengo hambre")
+alert("Ya tengo hambre")
+alert("Ya tengo hambre") */
+
+/*
+
+  Las lineas comentadas el navegador las ignora y no se ejecutan 
+
+*/
